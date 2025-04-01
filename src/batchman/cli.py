1c1d7e3e16a242f820a1d4cdd51c52fd6df0f1b3
havetc <corentin.havet@hotmail.fr>
2025-04-01 14:24:20 +0200

    [DEV] new cli functionnalities
    adding a reload, which automatically happen in case of batch change
    can also download a complete batch from the cli

--- src/batchman/cli.py
+++ src/batchman/cli.py
@@ -1,12 +1,7 @@
 import click
 from pathlib import Path
 from typing import Optional
-from itertools import chain
-import functools
-from prettytable import PrettyTable
 
-from .batch_interfaces import UploadedBatch, EditableBatch, DownloadedBatch
-from .batchman import Batcher, LocalBatchStatus
 from .utils.ui import TableApp
 from .utils.logging import logger
 
@@ -17,57 +12,7 @@ from .utils.logging import logger
 def cli(dir: Optional[str]) -> None:
     """Manage batches of requests."""
 
-    batcher = Batcher(batches_dir=Path(dir))
-
-    errors = []
-
-    editable_batches, uploaded_batches, downloaded_batches, listing_errors = batcher.list_batches()
-
-    errors.extend(listing_errors)
-
-    changed_batches = []
-
-    waiting_batches = [batch for batch in uploaded_batches if batch.status in [LocalBatchStatus.VALIDATING, LocalBatchStatus.REGISTERED, LocalBatchStatus.IN_PROGRESS]]
-
-    if waiting_batches:
-        click.echo(
-            f"Found {len(editable_batches)+len(uploaded_batches)+len(downloaded_batches)} valid batches and {len(waiting_batches)} batches to synchronize: Syncing...  ",
-            nl=False,
-        )
-        statuses = {batch.unique_id: batch.status for batch in set(uploaded_batches + editable_batches + downloaded_batches)}
-        batcher.sync_batches()
-        editable_batches, uploaded_batches, downloaded_batches, listing_errors = batcher.list_batches()
-        errors = listing_errors
-        new_statuses = {batch.unique_id: batch.status for batch in set(uploaded_batches + editable_batches + downloaded_batches)}
-        changed_batches = {batch_id: (statuses[batch_id], new_statuses[batch_id]) for batch_id in statuses if statuses[batch_id] != new_statuses[batch_id]}
-        click.echo("Done")
-
-    else:
-        click.echo(f"Found {len(editable_batches)+len(uploaded_batches)+len(downloaded_batches)} batches.")
-
-    table = PrettyTable(["Local ID", "Name", "Status", "Provider", "Remote ID"])
-
-    fields_getters = [
-        ("unique_id", lambda batch: batch.params.unique_id),
-        ("name", lambda batch: batch.params.name),
-        ("status", lambda batch: batch.status.value),
-        ("provider", lambda batch: batch.params.provider.get("name", "N/A")),
-        ("remote_id", lambda batch: batch.remote_id or "N/A"),
-    ]
-
-    for batch in chain(editable_batches, uploaded_batches, downloaded_batches):
-        row = []
-        for field, getter in fields_getters:
-            try:
-                row.append(getter(batch))
-            except Exception as e:
-                row.append("ERROR")
-                errors.append(
-                    f"Batch {batch.params.name}:{batch.params.unique_id} - {field}: {e}"
-                )
-        table.add_row(row)
-
-    app = TableApp(table, batcher, changed_batches, errors)
+    app = TableApp(dir)
     app.run()
 
 
--- src/batchman/utils/ui.py
+++ src/batchman/utils/ui.py
@@ -1,8 +1,13 @@
 from pathlib import Path
-from typing import Dict, Tuple, List, TYPE_CHECKING
+from typing import Dict, Tuple, List
 from functools import partial
 from prettytable import PrettyTable
 from enum import Enum
+import click
+from itertools import chain
+import functools
+from prettytable import PrettyTable
+import asyncio
 
 from textual.app import App, ComposeResult
 from textual.widgets import DataTable, Footer, Button, Label, Markdown
@@ -12,9 +17,8 @@ from textual import events
 
 from ..providers.registry import ProviderRegistry
 from ..batch_interfaces import UploadedBatch
-
-if TYPE_CHECKING:
-    from ..batchman import Batcher
+from ..batch_interfaces import UploadedBatch, EditableBatch, DownloadedBatch
+from ..batchman import Batcher, LocalBatchStatus
 
 
 class MarkdownPopup(ModalScreen):
@@ -40,7 +44,7 @@ class PopupScreen(ModalScreen):
 
     def compose(self) -> ComposeResult:
         if self.action_confirm:
-            yield Grid(Label(self.message, id="question", markup=True), Label("Press Y to confirm", id="actionconfirm"), id="popup")
+            yield Grid(Label(self.message, id="question", markup=True), Label("Press y to confirm (anything else to cancel)", id="actionconfirm"), id="popup")
         else:
             yield Grid(Label(self.message, id="question"), id="popup")
 
@@ -129,19 +133,73 @@ Button {
 }"""
 
     BINDINGS = [
-        ("q", "quit", "Quit"),
-        ("c", "cancel", "Cancel"),
-        ("d", "delete", "Delete"),
-        ("p", "print", "Print paths"),
+        ("q", "quit", "Quit ╰┈➤🚪"),
+        ("c", "cancel", "Cancel 🚫"),
+        ("D", "delete", "Delete ❌"),
+        ("d", "download", "Download 📥"),
+        ("p", "print", "Print paths 📄"),
+        ("r", "reload", "Refresh table 🔄"),
     ]
 
-    def __init__(self, table: PrettyTable, batcher: "Batcher", changed_batches: Dict[str, Tuple[Enum, Enum]], errors: List[str]):
-        self.table = table
-        self.data_table = DataTable()
-        self.batcher = batcher
-        self.changed_batches = changed_batches
-        self.errors = errors
+    def __init__(self, dir: str):
+
+        self.dir = dir
         super().__init__()
+        self.data_table = DataTable()
+        self.batcher = Batcher(batches_dir=Path(self.dir))
+        # self.table, self.changed_batches, self.errors = self.set_table()
+
+
+    def set_table(self) -> Tuple[PrettyTable, Dict[str, Tuple[LocalBatchStatus, LocalBatchStatus]], List[str]]:
+        errors = self.batcher.sync_batches()
+        editable_batches, uploaded_batches, downloaded_batches, listing_errors = self.batcher.list_batches()
+
+        errors.extend(listing_errors)
+
+        changed_batches = []
+
+        waiting_batches = [batch for batch in uploaded_batches if batch.status in [LocalBatchStatus.VALIDATING, LocalBatchStatus.REGISTERED, LocalBatchStatus.IN_PROGRESS]]
+
+        if waiting_batches:
+            click.echo(
+                f"Found {len(editable_batches)+len(uploaded_batches)+len(downloaded_batches)} valid batches and {len(waiting_batches)} batches to synchronize: Syncing...  ",
+                nl=False,
+            )
+            statuses = {batch.unique_id: batch.status for batch in set(uploaded_batches + editable_batches + downloaded_batches)}
+            self.batcher.sync_batches()
+            editable_batches, uploaded_batches, downloaded_batches, listing_errors = self.batcher.list_batches()
+            errors = listing_errors
+            new_statuses = {batch.unique_id: batch.status for batch in set(uploaded_batches + editable_batches + downloaded_batches)}
+            changed_batches = {batch_id: (statuses[batch_id], new_statuses[batch_id]) for batch_id in statuses if statuses[batch_id] != new_statuses[batch_id]}
+            click.echo("Done")
+
+        else:
+            click.echo(f"Found {len(editable_batches)+len(uploaded_batches)+len(downloaded_batches)} batches.")
+
+        table = PrettyTable(["Local ID", "Name", "Status", "Provider", "Remote ID"])
+
+        fields_getters = [
+            ("unique_id", lambda batch: batch.params.unique_id),
+            ("name", lambda batch: batch.params.name),
+            ("status", lambda batch: batch.status.value),
+            ("provider", lambda batch: batch.params.provider.get("name", "N/A")),
+            ("remote_id", lambda batch: batch.remote_id or "N/A"),
+        ]
+
+        for batch in chain(editable_batches, uploaded_batches, downloaded_batches):
+            row = []
+            for field, getter in fields_getters:
+                try:
+                    row.append(getter(batch))
+                except Exception as e:
+                    row.append("ERROR")
+                    errors.append(
+                        f"Batch {batch.params.name}:{batch.params.unique_id} - {field}: {e}"
+                    )
+            table.add_row(row)
+
+        return table, changed_batches, errors
+
 
     @staticmethod
     def _req_fmt(req_uid: str) -> str:
@@ -152,6 +210,7 @@ Button {
         yield Footer()
 
     def re_compute(self):
+        self.table, self.changed_batches, self.errors = self.set_table()
         self.data_table.clear()
         self.data_table.cursor_type = "row"
         self.data_table.zebra_stripes = True
@@ -159,6 +218,8 @@ Button {
             self.data_table.add_columns(*self.table.field_names)
         for row in self.table.rows:
             self.data_table.add_row(*row, key=row[0])
+        self.query_one(DataTable).loading = False
+        # self.data_table.loading = False
 
     def on_mount(self) -> None:
         self.re_compute()
@@ -174,9 +235,9 @@ Button {
                 self.batcher.delete_batch(unique_id=batch_id)
                 self.push_screen(PopupScreen(f"Batch {self._req_fmt(batch_id)} deleted"))
                 self.table.del_row(row_index=row_index)
-                self.re_compute()
+                self.action_reload()
             except Exception as e:
-                self.push_screen(PopupScreen(f"Failed to delete batch {self._req_fmt(batch_id)}: {e}"))
+                self.push_screen(PopupScreen(f"Failed to delete batch {self._req_fmt(batch_id)}:\n{e}"))
 
     def cancelling_batch(self, batch_id: str, confirm: bool):
         if confirm:
@@ -188,8 +249,9 @@ Button {
                     try:
                         batch.cancel()
                         self.push_screen(PopupScreen(f"Canceling of batch {self._req_fmt(batch_id)} confirmed"))
+                        self.action_reload()
                     except Exception as e:
-                        self.push_screen(PopupScreen(f"Failed to cancel batch {self._req_fmt(batch_id)}: {e}"))
+                        self.push_screen(PopupScreen(f"Failed to cancel batch {self._req_fmt(batch_id)}:\n{e}"))
             else:
                 self.push_screen(PopupScreen(f"Batch {self._req_fmt(batch_id)} is not in a cancelable state"))
 
@@ -206,7 +268,7 @@ Button {
         table.cursor_type = "row"
         coord = table.cursor_coordinate.row
         req_uid = table.get_row_at(coord)[0]
-        self.push_screen(PopupScreen(f"Delete request {self._req_fmt(req_uid)}? It [bold]won't[/bold] cancel/remove it on the provider, "
+        self.push_screen(PopupScreen(f"[bold red]Delete[/bold red] request {self._req_fmt(req_uid)}? It [bold]won't[/bold] cancel/remove it on the provider, "
                                      "but it will remove it [bold]permanently[/bold] locally", action_confirm=True),
                          callback=partial(self.deleting_batch, req_uid, coord))
 
@@ -216,6 +278,33 @@ Button {
                                    f"* **Current dir:** `{Path.cwd()}`\n"
                                    f"* **Batches dir:** `{self.batcher.batches_dir.resolve()}`\n"
                                    f"* **Provider config file:** `{provider_config_file_path}`"))
+
+    def action_reload(self):
+        self.data_table.loading = True
+        self.call_after_refresh(self.re_compute)
+
+    def downloading_batch(self, batch_id: str, confirm: bool):
+        if confirm:
+            batch = self.batcher.load_batch(unique_id=batch_id)
+            if isinstance(batch, DownloadedBatch):
+                self.push_screen(PopupScreen(f"Batch {self._req_fmt(batch_id)} is already downloaded"))
+            elif isinstance(batch, EditableBatch):
+                self.push_screen(PopupScreen(f"Batch {self._req_fmt(batch_id)} has not been uploaded yet"))
+            else:
+                try:
+                    batch.download()
+                    self.push_screen(PopupScreen(f"Downloading batch {self._req_fmt(batch_id)}"))
+                    self.action_reload()
+                except Exception as e:
+                    self.push_screen(PopupScreen(f"Failed to download batch {self._req_fmt(batch_id)}:\n{e}"))
+
+    def action_download(self):
+        table = self.data_table
+        table.cursor_type = "row"
+        coord = table.cursor_coordinate.row
+        req_uid = table.get_row_at(coord)[0]
+        self.push_screen(PopupScreen(f"Download request {self._req_fmt(req_uid)}?", action_confirm=True),
+                         callback=partial(self.downloading_batch, req_uid))
 
     def action_quit(self):
         self.app.exit()
